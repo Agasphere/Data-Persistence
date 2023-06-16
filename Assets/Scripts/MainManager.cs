225aa352ec86f00c726fb29a21db1f35000c07c2
Agasphere <101063972+Agasphere@users.noreply.github.com>
2023-06-16 21:47:48 +0300

    Menu added
    A new scene with UI Elements that represent menu was added

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -12,25 +10,23 @@ public class MainManager : MonoBehaviour
 
     public Text ScoreText;
     public GameObject GameOverText;
-    
-    private bool m_Started = false;
-    private int m_Points;
-    
-    private bool m_GameOver = false;
 
-    
+    private bool m_Started;
+    private int m_Points;
+    private bool m_GameOver;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         const float step = 0.6f;
-        int perLine = Mathf.FloorToInt(4.0f / step);
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
-        for (int i = 0; i < LineCount; ++i)
+        var perLine = Mathf.FloorToInt(4.0f / step);
+
+        var pointCountArray = new[] { 1, 1, 2, 2, 5, 5 };
+        for (var i = 0; i < LineCount; ++i)
         {
-            for (int x = 0; x < perLine; ++x)
+            for (var x = 0; x < perLine; ++x)
             {
-                Vector3 position = new Vector3(-1.5f + step * x, 2.5f + i * 0.3f, 0);
+                var position = new Vector3(-1.5f + step * x, 2.5f + i * 0.3f, 0);
                 var brick = Instantiate(BrickPrefab, position, Quaternion.identity);
                 brick.PointValue = pointCountArray[i];
                 brick.onDestroyed.AddListener(AddPoint);
@@ -42,16 +38,15 @@ public class MainManager : MonoBehaviour
     {
         if (!m_Started)
         {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                m_Started = true;
-                float randomDirection = Random.Range(-1.0f, 1.0f);
-                Vector3 forceDir = new Vector3(randomDirection, 1, 0);
-                forceDir.Normalize();
+            if (!Input.GetKeyDown(KeyCode.Space)) return;
+            
+            m_Started = true;
+            var randomDirection = Random.Range(-1.0f, 1.0f);
+            var forceDir = new Vector3(randomDirection, 1, 0);
+            forceDir.Normalize();
 
-                Ball.transform.SetParent(null);
-                Ball.AddForce(forceDir * 2.0f, ForceMode.VelocityChange);
-            }
+            Ball.transform.SetParent(null);
+            Ball.AddForce(forceDir * 2.0f, ForceMode.VelocityChange);
         }
         else if (m_GameOver)
         {
@@ -62,7 +57,7 @@ public class MainManager : MonoBehaviour
         }
     }
 
-    void AddPoint(int point)
+    private void AddPoint(int point)
     {
         m_Points += point;
         ScoreText.text = $"Score : {m_Points}";
