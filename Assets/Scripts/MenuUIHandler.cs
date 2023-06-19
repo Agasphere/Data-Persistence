6997d557dcfaa9d742d3849a4ec8c99c361d15fc
Agasphere <101063972+Agasphere@users.noreply.github.com>
2023-06-19 12:52:45 +0300

    Added fade out logic for error message

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -1,6 +1,8 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 #if UNITY_EDITOR
 using UnityEditor;
@@ -10,6 +12,9 @@ using UnityEditor;
 public class MenuUIHandler : MonoBehaviour
 {
     [SerializeField] private TMP_InputField _inputField;
+    [SerializeField] private TMP_Text _warningText;
+    
+    [SerializeField] private float _warningTextDisplayTime = 1;
 
     private void Awake()
     {
@@ -19,10 +24,28 @@ public class MenuUIHandler : MonoBehaviour
     public void StartSession()
     {
         var playerName = _inputField.text;
-        if (playerName == string.Empty) return;
+        if (playerName == string.Empty)
+        {
+            StopCoroutine(nameof(DisplayWarning));
+            StartCoroutine(DisplayWarning(_warningText, _warningTextDisplayTime));
+            return;
+        }
 
         PersistenceManager.Instance.RegisterPlayer(playerName);
         SceneManager.LoadScene(1);
+    }
+
+    private static IEnumerator DisplayWarning(Graphic i, float t)
+    {
+        var color = i.color;
+        color.a = 1;
+
+        while (color.a > 0.0f)
+        {
+            color.a -= Time.deltaTime / t;
+            i.color = color;
+            yield return null;
+        }
     }
 
     public void Exit()
